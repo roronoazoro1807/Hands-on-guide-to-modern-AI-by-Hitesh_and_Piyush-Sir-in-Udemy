05df783bf85018b785867ec691b4583d2b347534
roronoazoro1807 <lovekd72@gmail.com>
2025-09-23 16:55:06 +0530

    made some changes on smart thermostat

--- 03_conditionals/smart_thermostat.py
+++ 03_conditionals/smart_thermostat.py
@@ -1,10 +1,15 @@
-device_status = "active"
-temperature = 38
+import random
 
-if device_status == "active":
-    if temperature > 35:
-        print("High temperature alert!")
-    else:
-        print("Temperature is normal")
+device_status = random.choice(["active", "offline"])
+temperature = random.randint(20,50)
+
+print("Device status:", device_status)
+print("Current temperature:", temperature)
+
+if device_status == "active" :
+  if temperature > 35:
+    print("High temperature alert")
+  else:
+    print("Temperature normal")
 else:
-    print("Device is offline")
+  print("device is offline")
